f58e41710b310b8fb5706371c92ef7b0cbdaf23b
Rostyslav <excoreky@gmail.com>
2021-02-28 22:34:30 +0200

    Lab 1 Individual Task done

---
+++ Lab_1_IndividualTask/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Lab_1_IndividualTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(52, 89);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(300, 60);
+            this.textBox.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(504, 92);
+            this.label.MaximumSize = new System.Drawing.Size(300, 0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(54, 25);
+            this.label.TabIndex = 1;
+            this.label.Text = "Test";
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(323, 317);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(145, 60);
+            this.buttonCopy.TabIndex = 2;
+            this.buttonCopy.Text = "Copy text";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Individual var 12";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button buttonCopy;
+    }
+}
+
---
+++ Lab_1_IndividualTask/Form1.cs
@@ -0,0 +1,40 @@
+﻿// Var 12
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_1_IndividualTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            label.Text = textBox.Text;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.DarkViolet;
+            textBox.Text = "";
+            label.Text = "Початок роботи";
+            label.ForeColor = Color.LightBlue;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label.Text = "";
+        }
+    }
+}
