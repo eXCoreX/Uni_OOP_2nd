bdd0d821315ecd25770f71fd47e94319185927d1
Rostyslav <excoreky@gmail.com>
2021-02-28 20:42:10 +0200

    Lab 1 Common Task 3 done

---
+++ Lab_1_ComTask_3/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Lab_1_ComTask_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // colorListBox
+            // 
+            this.colorListBox.FormattingEnabled = true;
+            this.colorListBox.ItemHeight = 25;
+            this.colorListBox.Items.AddRange(new object[] {
+            "Red",
+            "Yellow",
+            "Blue"});
+            this.colorListBox.Location = new System.Drawing.Point(12, 47);
+            this.colorListBox.Name = "colorListBox";
+            this.colorListBox.Size = new System.Drawing.Size(120, 79);
+            this.colorListBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Fill Color";
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(138, 131);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(650, 307);
+            this.panel.TabIndex = 2;
+            this.panel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.colorListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox colorListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel;
+    }
+}
+
---
+++ Lab_1_ComTask_3/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_1_ComTask_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Color color;
+            color = Color.Green;
+
+            if (panel.BackColor != color)
+            {
+                panel.BackColor = color;
+                panel.Invalidate();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            colorListBox.SelectedIndex = 0;
+        }
+
+        private void panel_MouseMove(object sender, MouseEventArgs e)
+        {
+            Color color;
+            switch (colorListBox.SelectedItem)
+            {
+                case "Red":
+                    color = Color.Red;
+                    break;
+
+                case "Yellow":
+                    color = Color.Yellow;
+                    break;
+
+                case "Blue":
+                    color = Color.Blue;
+                    break;
+
+                default:
+                    throw new Exception("Bad selected item");
+            }
+
+            if (panel.BackColor != color)
+            {
+                panel.BackColor = color;
+                panel.Invalidate();
+            }
+        }
+    }
+}
