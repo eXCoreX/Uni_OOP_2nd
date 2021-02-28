ae94d8a23f7c1439c14d7160ef0a9a11d3d8b4e7
Rostyslav <excoreky@gmail.com>
2021-02-28 20:02:56 +0200

    Lab 2 Common Task 2 done

---
+++ Lab_1_ComTask_2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_1_ComTask_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDeleteLabels_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Controls.Count; )
+            {
+                var comp = Controls[i];
+                if (comp is Label)
+                {
+                    Controls.Remove(comp);
+                }
+                else
+                {
+                    ++i;
+                }
+            }
+        }
+    }
+}
