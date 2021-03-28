b0203e384e1bb3ec969cb52329f6b578a3b1c2a4
Rostyslav <excoreky@gmail.com>
2021-03-28 19:35:34 +0300

    Lab 2 Task 2 done

---
+++ Lab 2/Lab_2_Task_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer(new TimeSpan(0, 0, 0, 0, 100), false, () =>
+               {
+                   Console.WriteLine("Tick");
+               });
+            Console.WriteLine("Enter 1 to start, 2 to pause, 3 to cancel timer.");
+
+            while (true)
+            {
+                string inputStr = Console.ReadLine();
+                int token;
+                if (int.TryParse(inputStr, out token))
+                {
+                    switch (token)
+                    {
+                        case 1:
+                            myTimer.Start();
+                            break;
+                        case 2:
+                            myTimer.Stop();
+                            break;
+                        case 3:
+                            myTimer.Cancel();
+                            goto outer;
+                        default:
+                            break;
+                    }
+                }
+            }
+        outer:;
+            Console.WriteLine("Press enter to exit" );
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 2/Lab_2_Task_2/Timer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab_2_Task_2
+{
+    class Timer
+    {
+        private Task timerThread;
+        private object _lock;
+        private CancellationTokenSource cancellationTokenSource;
+
+        public Action Action { get; private set; }
+
+        private TimeSpan _Interval;
+        public TimeSpan Interval
+        {
+            get
+            {
+                return _Interval;
+            }
+            set
+            {
+                if (value.TotalMilliseconds < 50)
+                {
+                    throw new ArgumentException("Inteval is too short, minimum is 50 ms");
+                }
+                _Interval = value;
+            }
+        }
+
+        private bool isActive = false;
+
+        public void Start()
+        {
+            if (!isActive)
+            {
+                isActive = true;
+                Monitor.Exit(_lock);
+            }
+        }
+
+        public void Stop()
+        {
+            if (isActive)
+            {
+                isActive = false;
+                Monitor.Enter(_lock);
+            }
+        }
+
+        public void Cancel()
+        {
+            cancellationTokenSource.Cancel();
+        }
+
+        public Timer(TimeSpan interval, bool start, Action action)
+        {
+            Action = action;
+            Interval = interval;
+            _lock = new object();
+            cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken ct = cancellationTokenSource.Token;
+            Monitor.Enter(_lock);
+            timerThread = Task.Run(() =>
+            {
+                while(true)
+                {
+                    if (ct.IsCancellationRequested)
+                    {
+                        return;
+                    }
+                    lock (_lock)
+                    {
+                        Action();
+                    }
+                    Thread.Sleep(interval);
+                }
+            }, ct);
+
+            if (start)
+            {
+                Start();
+            }
+        }
+    }
+}
