89522356c1fd5f03c1ae165b8208a3c0f079a86b
eXCoreX <excoreky@gmail.com>
2021-03-28 21:00:06 +0300

    Lab 2 Task 4 done

---
+++ Lab 2/Lab_2_Task_4/InfiniteSeriesCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_Task_4
+{
+    class InfiniteSeriesCalculator
+    {
+        public static double Calculate(Func<int, double> nthTerm, int n_0 = 0, double eps = 1e-7, int maxIter = 10_000)
+        {
+            double res = 0;
+            int n = n_0;
+            int iter = 0;
+            double currentEps;
+            do
+            {
+                double newRes = res + nthTerm(n);
+                n++;
+                iter++;
+                currentEps = res - newRes;
+                res = newRes;
+            } while (Math.Abs(currentEps) > eps && iter < maxIter);
+
+            return res;
+        }
+    }
+}
---
+++ Lab 2/Lab_2_Task_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_Task_4
+{
+    class Program
+    {
+        static double Factorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Can't find factorial of negative value.");
+            }
+            double res = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                res *= i;
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Series 1 (1/2^n, n_0 = 0):");
+            Console.WriteLine(InfiniteSeriesCalculator.Calculate(i => 1 / Math.Pow(2, i), 0, 0.01));
+            Console.WriteLine("Series 2 (1/n!, n_0 = 1):");
+            Console.WriteLine(InfiniteSeriesCalculator.Calculate(i => 1 / Factorial(i), 0, 0.01));
+            Console.WriteLine("Series 3 (1 + (-1)^n/2^(n+1), n_0 = 0):");
+            Console.WriteLine(1 + InfiniteSeriesCalculator.Calculate(i => Math.Pow(-1, i) / Math.Pow(2, i + 1), 0, 0.01));
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
